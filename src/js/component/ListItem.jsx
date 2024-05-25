4d7aa842181551fd88cfd89be6b08788e9b67059
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-05-25 00:31:37 +0000

    finished

--- src/js/component/ListItem.jsx
+++ src/js/component/ListItem.jsx
@@ -4,12 +4,12 @@ import React from "react";
 
 const ListItem = (props) => {
 	return (
-		props.list.map((item) => {
+		props.list.map((item, index) => {
 			return (
-				<div className="d-flex justify-content-between">
-					<li key={item.id} className="list-group-item w-100 border-end-0 border-start py-3">{item}</li>
+				<li key={item + index} className="d-flex justify-content-between">
+					<span className="list-group-item w-100 border-end-0 border-start py-3">{item}</span>
 					<button onClick={() => props.remove(item)} className="btn border border border-start-0 rounded-0">X</button>
-				</div>
+				</li>
 			)
 		})
 	)
